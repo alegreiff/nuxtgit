de7d6cf5b32be73316eb34dcf0a85d9ae60cb7a1
Jaime de Greiff <alegreiff@gmail.com>
2018-03-29 11:19:43 -0500

    logosass

--- pages/jaime.vue
+++ pages/jaime.vue
@@ -1,26 +1,10 @@
 <template>
 <div class="grid">
 <div>Inicio</div>
+<div>Lateral</div>
 <div>Inicio</div>
 <div>Inicio</div>
-<div>Inicio</div>
-<div>Inicio</div>
+<div><img src="~/assets/sass.svg" alt="" width="220"></div>
 <div>FIN</div>
 </div>
 </template>
-<style lang="scss">
-$color: red;
-div.grid{
-    display: grid;
-    padding: 1em;
-    /*background-color: rgb(35, 81, 230);*/
-    grid-template-columns: 1fr 2fr;
-    grid-gap: 1em;
-}
-div.grid div{
-    background-color: rgb(43, 101, 245);
-    padding: 2em;
-    color: lighten($color, 23%);
-    font-size: 1.6em;
-}
-</style>
