19329720c332d71cab51fe6e4bbe7f3df24aab7d
Jaime de Greiff <alegreiff@gmail.com>
2018-03-29 11:01:43 -0500

    azules

--- pages/jaime.vue
+++ pages/jaime.vue
@@ -4,18 +4,22 @@
 <div>Inicio</div>
 <div>Inicio</div>
 <div>Inicio</div>
+<div>Inicio</div>
+<div>FIN</div>
 </div>
 </template>
 <style>
 div.grid{
     display: grid;
     padding: 1em;
-    background-color: aquamarine;
+    background-color: rgb(35, 81, 230);
     grid-template-columns: 1fr 2fr;
     grid-gap: 1em;
 }
 div.grid div{
-    background-color: red;
+    background-color: rgb(43, 101, 245);
     padding: 2em;
+    color: aliceblue;
+    font-size: 1.6em;
 }
 </style>
