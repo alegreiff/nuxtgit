815abecc754c03455127ab57df97aef4d1b7d780
Jaime de Greiff <alegreiff@gmail.com>
2018-03-29 11:13:21 -0500

    variables sass

--- pages/jaime.vue
+++ pages/jaime.vue
@@ -8,18 +8,19 @@
 <div>FIN</div>
 </div>
 </template>
-<style>
+<style lang="scss">
+$color: red;
 div.grid{
     display: grid;
     padding: 1em;
-    background-color: rgb(35, 81, 230);
+    /*background-color: rgb(35, 81, 230);*/
     grid-template-columns: 1fr 2fr;
     grid-gap: 1em;
 }
 div.grid div{
     background-color: rgb(43, 101, 245);
     padding: 2em;
-    color: aliceblue;
+    color: lighten($color, 23%);
     font-size: 1.6em;
 }
 </style>
